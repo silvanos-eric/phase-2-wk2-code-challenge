791c39949a92b8ec53e736d8100969d6f4cd22c5
Silvanos Eric <ericoderito@gmail.com>
2024-08-10 12:14:29 +0300

    feat(app): fetch bot collection data and display it in BotCollection component
    - Added `useEffect` in `App` to fetch bot collection data from API
    - Passed fetched data as a prop to `BotCollection` component
    - Updated `BotCollection` to display bot data in a table format

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import { BotCollection, BotDetails, YourBotArmy } from "./components";
 import "./App.css";
 
 function App() {
+  const [botCollectionData, setBotCollectionData] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/bots")
+      .then((response) => response.json())
+      .then(setBotCollectionData)
+      .catch(console.error);
+  }, []);
+
   return (
     <main>
-      <BotCollection />
+      <BotCollection collection={botCollectionData} />
       <BotDetails />
       <YourBotArmy />
     </main>
--- src/components/BotCollection.jsx
+++ src/components/BotCollection.jsx
@@ -1,3 +1,24 @@
-const BotCollection = () => <p>BotCollection</p>;
+const BotCollection = ({ collection }) => (
+  <table>
+    <thead>
+      <tr>
+        <th>ID</th>
+        <th>Name</th>
+        <th>Avatar</th>
+      </tr>
+    </thead>
+    <tbody>
+      {collection.map(({ id, name, avatar_url }) => (
+        <tr key={id}>
+          <td>{id}</td>
+          <td>{name}</td>
+          <td>
+            <img src={avatar_url} alt="" />
+          </td>
+        </tr>
+      ))}
+    </tbody>
+  </table>
+);
 
 export { BotCollection };
