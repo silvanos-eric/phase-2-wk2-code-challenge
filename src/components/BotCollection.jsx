6160543987cf9f8e8a5cb1864542c80ec0bc603e
Silvanos Eric <ericoderito@gmail.com>
2024-08-10 12:56:06 +0300

    feat(bot-collection): add bot selection on row click
    - Introduced `onSelect` prop to handle bot selection in `BotCollection`
    - Added click event on table rows to trigger bot selection
    - Passed the index of the selected bot to the `onSelect` callback
    -

--- src/components/BotCollection.jsx
+++ src/components/BotCollection.jsx
@@ -1,24 +1,29 @@
-const BotCollection = ({ collection }) => (
-  <table>
-    <thead>
-      <tr>
-        <th>ID</th>
-        <th>Name</th>
-        <th>Avatar</th>
-      </tr>
-    </thead>
-    <tbody>
-      {collection.map(({ id, name, avatar_url }) => (
-        <tr key={id}>
-          <td>{id}</td>
-          <td>{name}</td>
-          <td>
-            <img src={avatar_url} alt="" />
-          </td>
+const BotCollection = ({ collection, onSelect }) => {
+  const showBotDetails = (index) => {
+    onSelect(index);
+  };
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Avatar</th>
         </tr>
-      ))}
-    </tbody>
-  </table>
-);
+      </thead>
+      <tbody>
+        {collection.map(({ id, name, avatar_url }, index) => (
+          <tr key={id} onClick={() => showBotDetails(index)}>
+            <td>{id}</td>
+            <td>{name}</td>
+            <td>
+              <img src={avatar_url} alt="" />
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
 
 export { BotCollection };
